b6dc920453a85e08ded0edfbfd77522ce5cc5fe3
Michael Osarodion <macmive@gmail.com>
2020-08-25 16:46:29 +0100

    updated movie_from_json()

--- movie.py
+++ movie.py
@@ -15,7 +15,9 @@ class Movie:
             'watched': self.watched
         }
 
+    #  Argument unpacking: passing a dictionary as a set of named parameters
     @classmethod
-    def movie_from_json(cls, json_data):
-        return Movie(json_data['name'], json_data['genre'], json_data['watched'])
+    def movie_from_json(cls, json_data):  # format: {'name': '...', 'genre': '...', 'watched': '...'}
+        # return Movie(json_data['name'], json_data['genre'], json_data['watched'])
+        return Movie(**json_data)
 
